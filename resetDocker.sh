2d05924bdd64ce4421c6a6c55ffbd2065343b876
kameshX007 <kameshbook@gmail.com>
2022-09-25 15:09:40 +0530

    #

--- resetDocker.sh
+++ resetDocker.sh
@@ -5,5 +5,6 @@ echo $container;
 [ -z "$container" ] && echo "No running container found" || docker stop $(docker container ls -q);
 echo "Deleting all docker data...";
 docker system prune -a -f;
+sudo docker network create tunnel;
 echo "Docker reset successfull...";
 exit 0
